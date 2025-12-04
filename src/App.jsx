7f61792aaf962a8a886f680129810fc9e8cb0df4
Anoop Kumar <iamanoopav@gmail.com>
2025-12-04 16:03:27 +0530

    component added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroBanner from './Components/HeroBanner.jsx'
+
+const App = () => {
+  return (
+    <>
+        <h1>Hello, React!</h1>
+        <HeroBanner />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HeroBanner() {
+  return (
+    <section className="hero-banner">
+      <div className="container">
+        <h1>Welcome to Our Website</h1>
+        <p>Your journey to learning React starts here.</p>
+        <button className="cta-button">Get Started</button>
+      </div>
+    </section>
+  )
+}
+
+export default HeroBanner;
